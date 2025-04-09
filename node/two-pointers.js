7fb87d55d2bec1b3df7b734c5b01357b9faf2ecf
Juans <sebastian.ochoa.dev@gmail.com>
2025-04-09 15:50:26 +0200

    feat: reverseString

--- node/two-pointers.js
+++ node/two-pointers.js
@@ -42,32 +42,78 @@ For example, given nums = [1, 2, 4, 6, 8, 9, 14, 15] and target = 13, return tru
 /**
  * Example 3: Given two sorted integer arrays arr1 and arr2, return a new array that combines both of them and is also sorted.
  */
-function combine(arr1, arr2) {
-  let ans = [];
-  let i = 0,
-    j = 0;
+// function combine(arr1, arr2) {
+//   let ans = [];
+//   let i = 0,
+//     j = 0;
 
-  while (i < arr1.length && j < arr2.length) {
-    if (arr1[i] < arr2[j]) {
-      ans.push(arr1[i]);
-      i++;
-    } else {
-      ans.push(arr2[j]);
-      j++;
-    }
-  }
+//   while (i < arr1.length && j < arr2.length) {
+//     if (arr1[i] < arr2[j]) {
+//       ans.push(arr1[i]);
+//       i++;
+//     } else {
+//       ans.push(arr2[j]);
+//       j++;
+//     }
+//   }
 
-  while (i < arr1.length) {
-    ans.push(arr1[i]);
-    i++;
-  }
+//   while (i < arr1.length) {
+//     ans.push(arr1[i]);
+//     i++;
+//   }
 
-  while (j < arr2.length) {
-    ans.push(arr2[j]);
-    j++;
-  }
+//   while (j < arr2.length) {
+//     ans.push(arr2[j]);
+//     j++;
+//   }
 
-  return ans;
-}
+//   return ans;
+// }
 
-combine([1, 4, 7, 20], [3, 5, 6]);
+// combine([1, 4, 7, 20], [3, 5, 6]);
+
+/**
+ * Example 4: 392. Is Subsequence.
+
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+A subsequence of a string is a sequence of characters that can be obtained by deleting some (or none) of the characters from the original string, while maintaining the relative order of the remaining characters. For example, "ace" is a subsequence of "abcde" while "aec" is not.
+ */
+
+// function isSubsequence(s, t) {
+//   let i = 0,
+//     j = 0;
+
+//   while (i < s.length && j < t.length) {
+//     if (s[i] === t[j]) {
+//       i++;
+//     }
+
+//     j++;
+//   }
+
+//   return i === s.length;
+// }
+
+// console.log(isSubsequence("abcd", "abcdrere"));
+
+/**
+ * Revert a string using two pointers
+ */
+// function revertString(str) {
+//   let left = 0,
+//     right = str.length - 1;
+
+//   const arr = str.split("");
+
+//   while (left < right) {
+//     [arr[left], arr[right]] = [arr[right], arr[left]];
+
+//     left++;
+//     right--;
+//   }
+
+//   return arr.join("");
+// }
+
+// console.log(revertString("HolaMundo"));
--- python/two-pointers.py
+++ python/two-pointers.py
@@ -24,27 +24,53 @@
 
 # print(checkForTarget([1,2,4,5], 6))
 
-def combine(arr1:list, arr2:list)->list:
-    result = []
-    i = j = 0
+# def combine(arr1:list, arr2:list)->list:
+#     result = []
+#     i = j = 0
 
-    while i<len(arr1) and j<len(arr2):
-        if arr1[i] < arr2[j]:
-            result.append(arr1[i])
-            i+=1
-        else:
-            result.append(arr2[j])
-            j+=1
+#     while i<len(arr1) and j<len(arr2):
+#         if arr1[i] < arr2[j]:
+#             result.append(arr1[i])
+#             i+=1
+#         else:
+#             result.append(arr2[j])
+#             j+=1
     
 
-    while i < len(arr1):
-        result.append(arr1[i])
-        i+=1
+#     while i < len(arr1):
+#         result.append(arr1[i])
+#         i+=1
     
-    while j< len(arr2):
-        result.append(arr2[j])
-        j+=1
+#     while j< len(arr2):
+#         result.append(arr2[j])
+#         j+=1
 
-    return result
+#     return result
 
-print(combine([1,3,4], [2,4,6,9]))
+# print(combine([1,3,4], [2,4,6,9]))
+
+# def isSubsequence(s:str,t:str )->bool:
+#     i = j = 0
+
+#     while i < len(s) and j < len(t):
+#         if s[i] == t[j]:
+#             i+=1
+#         j+=1
+
+#     return i == len(s)
+
+# print(isSubsequence("abc", "ahbgdc"))
+
+# def reverseString(s:str)->str:
+#     left = 0
+#     right = len(s) - 1
+#     chars = list(s)
+
+#     while left < right:
+#         chars[left], chars[right] = chars[right], chars[left]
+#         left += 1
+#         right -= 1
+
+#     return ''.join(chars)
+
+# print(reverseString('HelloWorld'))
