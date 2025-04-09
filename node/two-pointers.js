eb831b64c2b7755a7004500fc2817ed2545bba9a
Juans <sebastian.ochoa.dev@gmail.com>
2025-04-09 11:12:45 +0200

    feat: Check for Target sum

--- node/two-pointers.js
+++ node/two-pointers.js
@@ -3,14 +3,38 @@
 A string is a palindrome if it reads the same forward as backward. That means, after reversing it, it is still the same string. For example: "abcdcba", or "racecar".
  */
 
-function isPalindrome(string = "") {
-  let left = 0,
-    right = string.length - 1;
-  while (left < right) {
-    if (string[left] !== string[right]) return false;
-    left++;
-    right--;
-  }
+// function isPalindrome(string = "") {
+//   let left = 0,
+//     right = string.length - 1;
+//   while (left < right) {
+//     if (string[left] !== string[right]) return false;
+//     left++;
+//     right--;
+//   }
 
-  return true;
-}
+//   return true;
+// }
+
+/**
+ * Example 2: Given a sorted array of unique integers and a target integer, return true if there exists a pair of numbers that sum to target, false otherwise. This problem is similar to Two Sum. (In Two Sum, the input is not sorted).
+
+For example, given nums = [1, 2, 4, 6, 8, 9, 14, 15] and target = 13, return true because 4 + 9 = 13.
+ */
+
+// function checkForTarget(arr, target) {
+//   let left = 0,
+//     right = arr.length - 1;
+
+//   while (left < right) {
+//     let current = arr[left] + arr[right];
+
+//     if (current === target) return true;
+
+//     if (current > target) right--;
+//     else left++;
+//   }
+
+//   return false;
+// }
+
+// console.log(checkForTarget([1, 2, 4, 6, 8, 9, 14, 15], 13));
--- python/two-pointers.py
+++ python/two-pointers.py
@@ -1,12 +1,25 @@
-def isPalindrome(s:str)->bool:
-    left = 0
-    right = len(s) -1
+# def isPalindrome(s:str)->bool:
+#     left = 0
+#     right = len(s) -1
 
-    while left < right:
-        if s[left] != s[right]: return False
-        left += 1
-        right -= 1
+#     while left < right:
+#         if s[left] != s[right]: return False
+#         left += 1
+#         right -= 1
 
-    return True
+#     return True
 
-print(isPalindrome("abba"))
+# print(isPalindrome("abba"))
+
+# def checkForTarget(arr:list, target:int)->bool:
+#     left = 0
+#     right = len(arr) -1
+
+#     while left< right:
+#         current = arr[left] + arr[right]
+#         if current == target: return True
+#         if current > target: right -= 1
+#         else: left += 1
+#     return False
+
+# print(checkForTarget([1,2,4,5], 6))
