a26e685530ed48c5e5bd944add0a7e468bc7410d
Juans <sebastian.ochoa.dev@gmail.com>
2025-04-11 15:53:06 +0200

    feat: Get max sum contiguos arrays

--- python/array.py
+++ python/array.py
@@ -3,8 +3,6 @@
 """
 Problem: Given an array of integers, find the maximum sum of a subarray with a fixed window size.
 """
-
-
 # def maxSubArraySum(nums: list, k: int) -> int:
 #     maxValue = float('-inf')
 #     windowLength = k - 1
@@ -22,3 +20,22 @@ Problem: Given an array of integers, find the maximum sum of a subarray with a f
 
 
 # maxSubArraySum([2, 1, 5, 1, 3, 2], 3)
+
+"""
+Maximum Sum Subarray of Size K
+Given an array of positive integers and a positive number k, find the maximum sum of any contiguous subarray of size k.
+"""
+# def getMaxSum(nums: list, k: int):
+#     maxSumValue = float('-inf')
+#     sumRunning = 0
+#     windowLength = k - 1
+
+#     for i in range(len(nums)):
+#         sumRunning += nums[i]
+
+#         if i >= windowLength:
+#             maxSumValue = max(maxSumValue, sumRunning)
+#             sumRunning -= nums[i - windowLength]
+#     return maxSumValue
+
+# print(getMaxSum([3, 5, 2, 1, 7, 2], 2))
